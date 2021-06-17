3a47dc02f3f9d09950b6d3922bab6752f03a5e71
hemingzhi <hemingzhi@kuaishou.com>
2021-06-17 17:23:38 +0800

    sync

--- run/preprocess/run.sh
+++ run/preprocess/run.sh
@@ -4,7 +4,7 @@ date="20210608"
 
 frag_size=200000
 data_file="${root}/data/${table}/${date}"
-data_config="${root}/run/preprocess/configs/xtr_base_no_emb.json"
+data_config="${root}/run/preprocess/configs/xtr_v1_no_sparse.json"
 vocab_save_path="${root}/data/vocab/${table}_${date}.pkl"
 
 python preprocess.py \
